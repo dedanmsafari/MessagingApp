3231f8c4e1a3ec89f0625fac1c7d9f8a28f29ac4
dedanmsafari <dedan.msafari@gmail.com>
2021-11-26 10:58:06 +0300

    introducing stream to the application

--- App.js
+++ App.js
@@ -1,8 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { connectUser } from "./src/services/streamChat/streamClient";
+
+import { StyleSheet, Text, View } from "react-native";
+
+const API_KEY = "mx3bf8x5sf2u";
 
 export default function App() {
+  React.useEffect(() => {
+    connectUser();
+    console.log("Connected");
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
@@ -14,8 +23,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
