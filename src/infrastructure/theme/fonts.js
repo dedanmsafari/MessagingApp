f61e9a10d3bac8b043859d967c237fd44d8e8aab
dedanmsafari <dedan.msafari@gmail.com>
2021-11-30 12:14:27 +0300

    created a fonts theme

---
+++ src/infrastructure/theme/fonts.js
@@ -0,0 +1,24 @@
+export const fonts = {
+  body: "Lato_400Regular", //Regular and Bold only
+  heading: "Fraunces_500Medium", // All Available
+  fancy: "PermanentMarker_400Regular", //Regular Only!
+};
+
+export const fontWeights = {
+  regular: 400,
+  medium: 500,
+  bold: 700,
+};
+
+// headers increasing by 2 and 4.
+export const fontSizes = {
+  caption: "12px",
+  button: "14px",
+  body: "16px",
+  title: "20px",
+  h5: "24px",
+  h4: "34px",
+  h3: "44px",
+  h2: "54px",
+  h1: "104px",
+};
