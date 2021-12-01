d827ad9965bf967f5c8e0f8145185494feb9377b
dedanmsafari <dedan.msafari@gmail.com>
2021-12-01 10:36:57 +0300

    Created the text component

---
+++ src/components/Text/text.component.js
@@ -0,0 +1,62 @@
+import styled from "styled-components";
+
+//Expected <Text variant ='hint'> Good Day Sir!</Text>
+
+const defaultTextStyle = (theme) => `
+  font-family:${theme.fonts.body};
+  font-weight:${theme.fontWeights.regular};
+  color:${theme.colors.text.primary};
+  flex-wrap:wrap;
+  margin-top:0px;
+  margin-bottom:0px;
+`;
+
+const body = (theme) => `
+font-size: ${theme.fontSizes.body};
+`;
+
+const hint = (theme) => `
+font-size: ${theme.fontSizes.body};
+`;
+
+const error = (theme) => `
+color: ${theme.colors.text.error};
+font-size: ${theme.fontSizes.heading};
+font-weight: ${theme.fontWeights.bold};
+`;
+
+const caption = (theme) => `
+font-size: ${theme.fontSizes.caption};
+font-weight: ${theme.fontWeights.bold};
+`;
+
+const label = (theme) => `
+font-family: ${theme.fonts.heading};
+font-size: ${theme.fontSizes.body};
+font-weight: ${theme.fontWeights.regular};
+`;
+const fancy = (theme) => `
+font-family: ${theme.fonts.fancy};
+font-size: ${theme.fontSizes.body};
+font-weight: ${theme.fontWeights.regular};
+`;
+
+const variants = {
+  body,
+  label,
+  caption,
+  error,
+  hint,
+  fancy,
+};
+
+export const Text = styled.Text`
+  ${({ theme }) => defaultTextStyle(theme)}
+  ${({ variant, theme }) => variants[variant](theme)}
+`;
+
+
+
+Text.defaultProps = {
+  variant: "body",
+};
