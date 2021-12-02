a59877ed8a5e36819b85fff936d26a78c46b82bd
dedanmsafari <dedan.msafari@gmail.com>
2021-12-02 14:27:36 +0300

    modified the index navigator

---
+++ src/infrastructure/navigation/index.js
@@ -0,0 +1,11 @@
+import { NavigationContainer } from "@react-navigation/native";
+import React from "react";
+import { AccountNavigator } from "./account.navigator";
+
+export const Navigation = () => {
+  return (
+    <NavigationContainer>
+      <AccountNavigator />
+    </NavigationContainer>
+  );
+};
