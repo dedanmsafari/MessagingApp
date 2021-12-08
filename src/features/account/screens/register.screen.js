ad91a0ce727dc4d5435309e00f3bef34c938041b
dedanmsafari <dedan.msafari@gmail.com>
2021-12-08 11:06:11 +0300

    modified the register screen

--- src/features/account/screens/register.screen.js
+++ src/features/account/screens/register.screen.js
@@ -1,7 +1,56 @@
-import React from 'react';
+import React, { useState } from "react";
+import {
+  AccountBackground,
+  AccountCover,
+  FormInput,
+  AuthButton,
+} from "../components/account.styles";
+import { Spacer } from "../../../components/Spacer/spacer.component";
+import { Text } from "../../../components/Text/text.component";
 
+export const RegisterScreen = ({ navigation }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [repeatPassword, setRepeatPassword] = useState("");
+  return (
+    <AccountBackground>
+      <AccountCover />
+      <Text variant="title">Register</Text>
+      <Spacer size="large" />
+      <FormInput
+        label="Email"
+        value={email}
+        textContentType="emailAddress"
+        autoCapitalize="none"
+        autoCorrect={false}
+        keyboardType="email-address"
+        onChangeText={(email) => setEmail(email)}
+      />
+      <Spacer size="large" />
 
-
-export const RegisterScreen = () => {
-  return null
-}
+      <FormInput
+        label="Password"
+        value={password}
+        textContentType="password"
+        autoCapitalize="none"
+        autoCorrect={false}
+        secureTextEntry
+        onChangeText={(password) => setPassword(password)}
+      />
+      <Spacer size="large" />
+      <FormInput
+        label="Repeat Password"
+        value={repeatPassword}
+        textContentType="password"
+        autoCapitalize="none"
+        autoCorrect={false}
+        secureTextEntry
+        onChangeText={(repeatPassword) => setRepeatPassword(repeatPassword)}
+      />
+      <Spacer size="large" />
+      <AuthButton mode="contained" onPress={() => navigation.goBack()}>
+        Back
+      </AuthButton>
+    </AccountBackground>
+  );
+};
