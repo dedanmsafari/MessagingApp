6d07aef5967b5b742ba072d5930a6410e4c0e907
dedanmsafari <dedan.msafari@gmail.com>
2021-12-08 11:05:49 +0300

    modified the login Screen

--- src/features/account/screens/login.screen.js
+++ src/features/account/screens/login.screen.js
@@ -1,5 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
+import {
+  AccountBackground,
+  AccountCover,
+  FormInput,
+  AuthButton,
+} from "../components/account.styles";
+import { Spacer } from "../../../components/Spacer/spacer.component";
+import { Text } from "../../../components/Text/text.component";
+export const LoginScreen = ({ navigation }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  return (
+    <AccountBackground>
+      <AccountCover />
+      <Text variant="title">login</Text>
+      <Spacer size="large" />
+      <FormInput
+        label="Email"
+        value={email}
+        textContentType="emailAddress"
+        autoCapitalize="none"
+        autoCorrect={false}
+        keyboardType="email-address"
+        onChangeText={(email) => setEmail(email)}
+      />
+      <Spacer size="large" />
 
-export const LoginScreen = () => {
-  return null;
+      <FormInput
+        label="Password"
+        value={password}
+        textContentType="password"
+        autoCapitalize="none"
+        autoCorrect={false}
+        secureTextEntry
+        onChangeText={(password) => setPassword(password)}
+      />
+      <Spacer size="large" />
+      <AuthButton mode="contained" onPress={() => navigation.goBack()}>
+        Back
+      </AuthButton>
+    </AccountBackground>
+  );
 };
