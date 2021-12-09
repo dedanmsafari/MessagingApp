30639aae3508707c21771b9deb86795f2e0b57af
dedanmsafari <dedan.msafari@gmail.com>
2021-12-09 08:54:16 +0300

    Login screen modification

--- src/features/account/screens/login.screen.js
+++ src/features/account/screens/login.screen.js
@@ -1,15 +1,22 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import {
   AccountBackground,
   AccountCover,
   FormInput,
   AuthButton,
+  ErrorContainer,
 } from "../components/account.styles";
 import { Spacer } from "../../../components/Spacer/spacer.component";
 import { Text } from "../../../components/Text/text.component";
+import { LoadingIndicator } from "../../../components/ActivityIndicator/loadingIndicator.component";
+import { AuthenticationContext } from "../../../services/authentication/authentication.context";
+
 export const LoginScreen = ({ navigation }) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+
+  const { login, error, isLoading } = useContext(AuthenticationContext);
+
   return (
     <AccountBackground>
       <AccountCover />
@@ -35,6 +42,23 @@ export const LoginScreen = ({ navigation }) => {
         secureTextEntry
         onChangeText={(password) => setPassword(password)}
       />
+      {error && (
+        <ErrorContainer>
+          <Text variant="error">{error}</Text>
+        </ErrorContainer>
+      )}
+      <Spacer size="large" />
+      {!isLoading ? (
+        <AuthButton
+          icon="lock-open-outline"
+          mode="contained"
+          onPress={() => login(email, password)}
+        >
+          Login
+        </AuthButton>
+      ) : (
+        <LoadingIndicator />
+      )}
       <Spacer size="large" />
       <AuthButton mode="contained" onPress={() => navigation.goBack()}>
         Back
