7d5c7d35a25eec0af519c71ab5282fa094d67187
dedanmsafari <dedan.msafari@gmail.com>
2022-01-31 16:07:07 +0300

    modified the settings screen

--- src/features/settings/screens/settings.screens.js
+++ src/features/settings/screens/settings.screens.js
@@ -1,9 +1,30 @@
-import React, { useContext } from "react";
-import { Text } from "../../../components/Text/text.component";
+import React, { useContext, useEffect, useState } from "react";
+import { View, FlatList } from "react-native";
 import { AuthenticationContext } from "../../../services/authentication/authentication.context";
 import { AuthButton } from "../../account/components/account.styles";
+import { chatClient } from "../../../services/streamChat/streamClient";
+import { UserListItem } from "../components/userListItem";
+import { SafeArea } from "../../../utils/safeArea.util.component";
 
 export const SettingsScreen = () => {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const response = await chatClient.queryUsers({});
+      setUsers(response.users);
+    };
+    fetchUsers();
+  }, []);
+
   const { logout } = useContext(AuthenticationContext);
-  return <AuthButton onPress={() => logout()}>log Out</AuthButton>;
+  return (
+    <View>
+      <AuthButton onPress={() => logout()}>log Out</AuthButton>
+      <FlatList
+        data={users}
+        renderItem={({ item }) => <UserListItem user={item} />}
+        keyExtractor={(item) => item.id}
+      />
+    </View>
+  );
 };
