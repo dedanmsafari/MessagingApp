cd43bb0c12ceb8892cb00d08b603d3e26d6ce1e6
dedanmsafari <dedan.msafari@gmail.com>
2022-02-01 16:32:30 +0300

    added Image component

--- src/components/Image/image.component.js
+++ src/components/Image/image.component.js
@@ -1,8 +1,8 @@
 import styled from "styled-components/native";
 
 export const Image = styled.Image`
-  width: 50;
-  height: 50;
+  width: 50px;
+  height: 50px;
   backgroundColor: ${({ theme }) => theme.colors.brand.tertiary};
   borderRadius: ${({ theme }) => theme.sizes[4]};
   marginRight:10px
