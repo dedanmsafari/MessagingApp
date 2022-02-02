5b85167c21e301f8fddc526a091702811fcff150
dedanmsafari <dedan.msafari@gmail.com>
2022-02-02 13:18:51 +0300

    userlist item component

--- src/features/settings/components/userListItem.js
+++ src/features/settings/components/userListItem.js
@@ -1,19 +1,36 @@
 import { Text } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 import styled from "styled-components/native";
-import React from "react";
+import React, { useContext } from "react";
 import { Image } from "../../../components/Image/image.component";
-const ListView = styled.View`
+import { AuthenticationContext } from "../../../services/authentication/authentication.context";
+const ListView = styled.TouchableOpacity`
   flex: 1;
   flex-direction: row;
   alignItems: center;
   margin: 10px;
 `;
 
-export const UserListItem = ({ user }) => {
+export const UserListItem = ({ chatuser, chatClient }) => {
+  const { user } = useContext(AuthenticationContext);
+  const navigation = useNavigation();
+
+  const onPress = async () => {
+    if (!chatuser.id || !user.uid) {
+      return;
+    }
+    const channel = chatClient.channel("messaging", {
+      members: [chatuser.id, user.uid],
+    });
+
+    await channel.watch();
+    navigation.navigate('PrivateMessages', { channel });
+  };
+
   return (
-    <ListView>
-      <Image source={{ uri: user.image }} />
-      <Text>{user.name}</Text>
+    <ListView onPress={onPress}>
+      <Image source={{ uri: chatuser.image }} />
+      <Text>{chatuser.name}</Text>
     </ListView>
   );
 };
