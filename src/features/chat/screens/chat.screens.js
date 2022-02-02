d4632f6c6fa6bd5220cddf7433aec66c0c2f5ec0
dedanmsafari <dedan.msafari@gmail.com>
2022-02-02 13:18:29 +0300

    chatscreen changes

--- src/features/chat/screens/chat.screens.js
+++ src/features/chat/screens/chat.screens.js
@@ -2,15 +2,16 @@ import React, { useContext } from "react";
 import { ChannelList } from "stream-chat-expo";
 import { LoadingIndicator } from "../../../components/ActivityIndicator/loadingIndicator.component";
 import { StreamChatContext } from "../../../services/streamChat/streamClient.context";
+import { AuthenticationContext } from "../../../services/authentication/authentication.context";
 
 export const ChatScreen = ({ navigation }) => {
   const { isReady } = useContext(StreamChatContext);
-
+  const { user } = useContext(AuthenticationContext);
   const channelPress = (channel) => {
     navigation.navigate("PrivateMessages", { channel });
   };
   const filters = {
-    members: { $in: ["YNLI3j2rZRfdn4NAdN5WOTZ82Dt1"] },
+    members: { $in: [`${user.uid}`] },
   };
   if (!isReady) {
     return <LoadingIndicator />;
